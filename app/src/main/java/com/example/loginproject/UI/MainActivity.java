03bf1f0f4351f8e53f424b31d617413048c36e83
Frank23 <frankagreda100@outlook.com>
2024-11-18 15:47:08 -0600

    Guia6

--- app/src/main/java/com/example/loginproject/UI/MainActivity.java
+++ app/src/main/java/com/example/loginproject/UI/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         btnAcercaDe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PoliticasDeActivity.class);
+                Intent intent = new Intent(MainActivity.this, AcercaDeActivity.class);
                 startActivity(intent);
             }
         });
